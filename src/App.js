a2bac5fd2ba9b05df92b41ba0870ee6fcfaf961c
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-09 20:51:59 +0100

    Add router and main container

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import MainContainer from "./containers/MainContainer";
 import "./style/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <MainContainer />
+    </Router>
   );
 }
 
