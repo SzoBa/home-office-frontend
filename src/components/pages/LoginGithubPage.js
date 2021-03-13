61da062e18470a1be9d071dc2c9c672d406eee4d
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-13 23:28:22 +0100

    Add github login page

---
+++ src/components/pages/LoginGithubPage.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import * as ENV from "../files/ENV.json";
+import { Link } from "react-router-dom";
+
+const LoginGithubPage = (props) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      headers: {
+        Accept: "application/json",
+      },
+      url: ENV.githubLoginCallback + props.location.search,
+    };
+    const getData = async () => {
+      const response = await axios(options);
+      setData(response.data);
+      console.log(response.data);
+    };
+    getData().catch((error) => {
+      console.log(error.response.data);
+    });
+  }, [props.location.search]);
+
+  return (
+    <div>
+      {data && (
+        <div>
+          <Link to="/">Back to main</Link>
+          <p>Logged in as</p>
+          <p>{data.username}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LoginGithubPage;
