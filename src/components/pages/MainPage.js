2d25f700f41045a28e92d3a81f63b5484372241d
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-13 23:28:52 +0100

    Add github login load url to main page

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -4,6 +4,7 @@ import * as ENV from "../files/ENV.json";
 
 export default function MainPage() {
   const [googleLoginUrl, setGoogleLoginUrl] = useState("");
+  const [githubLoginUrl, setGithubLoginUrl] = useState("");
 
   useEffect(() => {
     const options = {
@@ -21,11 +22,32 @@ export default function MainPage() {
     });
   }, []);
 
+  useEffect(() => {
+    const options = {
+      headers: {
+        Accept: "application/json",
+      },
+      url: ENV.githubLogin,
+    };
+    const getData = async () => {
+      const response = await axios(options);
+      setGithubLoginUrl(response.data.url);
+    };
+    getData().catch((error) => {
+      console.log(error.response.data);
+    });
+  }, []);
+
   return (
     <div>
       {googleLoginUrl && (
         <a className="App-link" href={googleLoginUrl}>
           Sign in with Google
+        </a>
+      )}
+      {githubLoginUrl && (
+        <a className="App-link" href={githubLoginUrl}>
+          Sign in with Github
         </a>
       )}
     </div>
