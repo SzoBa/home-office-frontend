a9a4b519656665a4949ff7f7d3d01a70e8446a80
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-15 21:43:42 +0100

    Change functionality to login page

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,55 +1,10 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import * as ENV from "../files/ENV.json";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function MainPage() {
-  const [googleLoginUrl, setGoogleLoginUrl] = useState("");
-  const [githubLoginUrl, setGithubLoginUrl] = useState("");
-
-  useEffect(() => {
-    const options = {
-      headers: {
-        Accept: "application/json",
-      },
-      url: ENV.googleLogin,
-    };
-    const getData = async () => {
-      const response = await axios(options);
-      setGoogleLoginUrl(response.data.url);
-    };
-    getData().catch((error) => {
-      console.log(error.response.data);
-    });
-  }, []);
-
-  useEffect(() => {
-    const options = {
-      headers: {
-        Accept: "application/json",
-      },
-      url: ENV.githubLogin,
-    };
-    const getData = async () => {
-      const response = await axios(options);
-      setGithubLoginUrl(response.data.url);
-    };
-    getData().catch((error) => {
-      console.log(error.response.data);
-    });
-  }, []);
-
   return (
     <div>
-      {googleLoginUrl && (
-        <a className="App-link" href={googleLoginUrl}>
-          Sign in with Google
-        </a>
-      )}
-      {githubLoginUrl && (
-        <a className="App-link" href={githubLoginUrl}>
-          Sign in with Github
-        </a>
-      )}
+      <Link to="/login">Login</Link>
     </div>
   );
 }
