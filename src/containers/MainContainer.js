c25d4e965e11968879443c834c6c3005d5e2ea8b
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-15 21:43:51 +0100

    Add login route

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import MainPage from "../components/pages/MainPage";
 import LoginGooglePage from "../components/pages/LoginGooglePage";
 import LoginGithubPage from "../components/pages/LoginGithubPage";
+import LoginPage from "../components/pages/LoginPage";
 
 export default function MainContainer() {
   return (
@@ -10,9 +11,9 @@ export default function MainContainer() {
       <div>
         <Route exact path="/" component={MainPage} />
         <Route path="/registration" component={MainPage} />
+        <Route path="/login" component={LoginPage} />
         <Route path="/auth/google" component={LoginGooglePage} />
         <Route path="/auth/github" component={LoginGithubPage} />
-        <Route path="/login" component={MainPage} />
         <Route path="/logout" component={MainPage} />
       </div>
     </div>
