ed3ab259496ee330e28255d75544ed10b0da17cc
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-17 20:39:36 +0100

    Add Redux style login to github login

--- src/components/pages/LoginGithubPage.js
+++ src/components/pages/LoginGithubPage.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import * as ENV from "../files/ENV.json";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { login } from "../../actions/index";
 
 const LoginGithubPage = (props) => {
   const [data, setData] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const options = {
@@ -16,12 +19,13 @@ const LoginGithubPage = (props) => {
     const getData = async () => {
       const response = await axios(options);
       setData(response.data);
+      dispatch(login(response.data));
       console.log(response.data);
     };
     getData().catch((error) => {
       console.log(error.response.data);
     });
-  }, [props.location.search]);
+  }, [props.location.search, dispatch]);
 
   return (
     <div>
