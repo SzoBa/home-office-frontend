716e276696cbac6eca8575800b46788e447693e7
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-17 20:41:51 +0100

    Add axios GET request hook

---
+++ src/hooks/UseGet.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useGetData = (url, token, setErrorMessage) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [fetchedData, setFetchedData] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json, text/plain, */*",
+        Authorization: "Bearer " + token,
+      },
+      url: url,
+    };
+    const getData = async () => {
+      const response = await axios(options);
+      setFetchedData(response.data);
+      setIsLoading(false);
+    };
+    getData().catch((error) => {
+      setErrorMessage(error.response.data);
+    });
+  }, [url, token, setErrorMessage]);
+
+  return [isLoading, fetchedData];
+};
+
+export default useGetData;
