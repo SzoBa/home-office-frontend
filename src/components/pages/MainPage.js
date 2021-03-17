1d2c856fb24088f0f1ad7ca2fb75bef571dbdc1b
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-17 20:41:26 +0100

    Add emails link to main page

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 export default function MainPage() {
   return (
     <div>
+      <Link to="/registration">Registration</Link>
       <Link to="/login">Login</Link>
+      <Link to="/email">Emails</Link>
     </div>
   );
 }
