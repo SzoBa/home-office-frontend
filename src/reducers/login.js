1392c9b757b46ccd806e1bfe46ecb2c3b25c1784
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-17 20:38:36 +0100

    Add login action

---
+++ src/reducers/login.js
@@ -0,0 +1,24 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const initialState = {
+  name: "",
+  sanctum_token: "",
+};
+
+const loginReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.LOGIN:
+      return {
+        name: action.payload.name,
+        sanctum_token: action.payload.token,
+      };
+
+    case actionTypes.LOGOUT:
+      return { initialState };
+
+    default:
+      return state;
+  }
+};
+
+export default loginReducer;
