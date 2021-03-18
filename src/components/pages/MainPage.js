3fd5f5de6c396e38180a2dd96272b0b8e053e159
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-18 12:27:39 +0100

    Add get location

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -2,6 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function MainPage() {
+  const geo = navigator.geolocation;
+  geo.getCurrentPosition((pos) => {
+    console.log(
+      "Lat: " + pos.coords.latitude + " - Long: " + pos.coords.longitude
+    );
+  });
+
   return (
     <div>
       <Link to="/registration">Registration</Link>
