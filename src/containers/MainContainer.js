34c777b97c5877e55eda9cfa27ae04cae7d466d6
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-19 18:21:58 +0100

    Add layout elements to main container

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,10 +5,17 @@ import LoginGooglePage from "../components/pages/LoginGooglePage";
 import LoginGithubPage from "../components/pages/LoginGithubPage";
 import LoginPage from "../components/pages/LoginPage";
 import EmailPage from "../components/pages/EmailPage";
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+import Navbar from "../components/layout/Navbar";
+import Sidebar from "../components/layout/Sidebar";
 
 export default function MainContainer() {
   return (
     <div>
+      <Header />
+      <Navbar />
+      <Sidebar />
       <div>
         <Route exact path="/" component={MainPage} />
         <Route path="/registration" component={MainPage} />
@@ -18,6 +25,7 @@ export default function MainContainer() {
         <Route path="/logout" component={MainPage} />
         <Route path="/email" component={EmailPage} />
       </div>
+      <Footer />
     </div>
   );
 }
