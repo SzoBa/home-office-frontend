8fbc066f632882ee184161cca977eb79cb69c5d4
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-21 20:06:06 +0100

    Change backto main to header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = (props) => {
   return (
     <div id="headerContainer">
+      {useLocation().pathname !== "/" ? <Link to="/">Back to main</Link> : ""}
       <Link to="/registration">Registration</Link>
       <Link to="/login">Login</Link>
     </div>
