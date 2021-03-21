297af327eb8e9fc71832ef3c87f02cade9d741a4
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-21 20:25:52 +0100

    Add Login form

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,39 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
+import UsePostData from "../../hooks/UsePostData";
 import * as ENV from "../files/ENV.json";
 
 export default function LoginPage() {
   const [googleLoginUrl, setGoogleLoginUrl] = useState("");
   const [githubLoginUrl, setGithubLoginUrl] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState([]);
+  const history = useHistory();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userObject = {
+      email: event.target.elements.email.value,
+      password: event.target.elements.password.value,
+    };
+    UsePostData(ENV.simpleLogin, "token, if exists", userObject, (response) => {
+      setErrorMessage([]);
+      if (response.status === 201) {
+        // setUser({
+        //   username: response.data.username,
+        //   token: response.data.token,
+        // });
+        return history.push("/");
+      }
+      console.log(response);
+      Object.entries(response).forEach(([k, v]) => {
+        v.forEach((value) => {
+          setErrorMessage((old) => [...old, value]);
+        });
+      });
+    });
+  };
 
   useEffect(() => {
     const options = {
@@ -40,18 +68,35 @@ export default function LoginPage() {
   }, []);
 
   return (
-    <div>
-      <Link to="/">Back to main</Link>
-      {googleLoginUrl && (
-        <a className="App-link" href={googleLoginUrl}>
-          Sign in with Google
-        </a>
-      )}
-      {githubLoginUrl && (
-        <a className="App-link" href={githubLoginUrl}>
-          Sign in with Github
-        </a>
-      )}
+    <div id="login_container">
+      <div id="content_div_login">
+        <h2>Login</h2>
+        <form method="post" onSubmit={handleSubmit}>
+          <label>Email:</label>
+          <input type="email" name="email" />
+          <label>Password: </label>
+          <input type="password" name="password" />
+          <button type="submit">Login</button>
+        </form>
+
+        {googleLoginUrl && (
+          <a className="App-link" href={googleLoginUrl}>
+            Sign in with Google
+          </a>
+        )}
+        {githubLoginUrl && (
+          <a className="App-link" href={githubLoginUrl}>
+            Sign in with Github
+          </a>
+        )}
+        {errorMessage === null
+          ? ""
+          : errorMessage.map((data, index) => (
+              <div id="error_msg" key={index}>
+                {data}
+              </div>
+            ))}
+      </div>
     </div>
   );
 }
