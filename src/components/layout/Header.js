e17d29a3d44bd7668f6496072bef6a508ed05366
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-22 19:02:47 +0100

    Refactor links

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = (props) => {
+  const login = useSelector((state) => state.login);
+
   return (
     <div id="headerContainer">
       {useLocation().pathname !== "/" ? <Link to="/">Back to main</Link> : ""}
+      <Link to="/email">Emails</Link>
       <Link to="/registration">Registration</Link>
       <Link to="/login">Login</Link>
+      {login.name ? (
+        <div>
+          <span>Logged in as: </span>
+          <span>{login.name}</span>
+        </div>
+      ) : (
+        <div>
+          <span>Not Logged in</span>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Navbar = (props) => {
   return (
     <div id="navbar_container">
-      <Link to="/email">Emails</Link>
+      <p>Email related</p>
     </div>
   );
 };
