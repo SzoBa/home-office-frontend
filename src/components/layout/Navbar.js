bac04e14b9a73d0b5eb839f02acef1620ffd9487
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-23 12:04:15 +0100

    Add hide navbar when not mail page

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Navbar = (props) => {
   return (
-    <div id="navbar_container">
+    <div
+      id="navbar_container_open"
+      className={useLocation().pathname === "/email" ? "" : "hide"}
+    >
       <p>Email related</p>
     </div>
   );
