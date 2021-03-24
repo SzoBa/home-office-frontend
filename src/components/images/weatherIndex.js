0a7588f2cd774be370986161aec749607d327280
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-24 13:04:54 +0100

    Add index file for weather images

---
+++ src/components/images/weatherIndex.js
@@ -0,0 +1,37 @@
+import afternoon_clouds from "./weather/afternoon_clouds.jpg";
+import afternoon_rain from "./weather/afternoon_rain.jpg";
+import afternoon_storm from "./weather/afternoon_storm.jpg";
+import afternoon_sun from "./weather/afternoon_sun.jpg";
+import freeze from "./weather/freeze.jpg";
+
+import midday_clouds from "./weather/midday_clouds.jpg";
+import midday_rain from "./weather/midday_rain.jpg";
+import midday_storm from "./weather/midday_storm.jpg";
+import midday_sun from "./weather/midday_sun.jpg";
+
+import morning_clouds from "./weather/morning_clouds.jpg";
+import morning_rain from "./weather/morning_rain.jpg";
+import morning_storm from "./weather/morning_storm.jpg";
+import morning_sun from "./weather/morning_sun.jpg";
+
+import night_clouds from "./weather/night_clouds.jpg";
+import night_rain from "./weather/night_rain.jpg";
+import night_storm from "./weather/night_storm.jpg";
+export {
+  afternoon_clouds,
+  afternoon_rain,
+  afternoon_storm,
+  afternoon_sun,
+  freeze,
+  midday_clouds,
+  midday_rain,
+  midday_storm,
+  midday_sun,
+  morning_clouds,
+  morning_rain,
+  morning_storm,
+  morning_sun,
+  night_clouds,
+  night_rain,
+  night_storm,
+};
