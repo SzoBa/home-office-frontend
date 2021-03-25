366f58e5b5ccd3c4f2a0163fa8c7e432a1a72787
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-25 20:24:59 +0100

    Add emails with sorting

--- src/components/pages/EmailPage.js
+++ src/components/pages/EmailPage.js
@@ -1,20 +1,158 @@
-import React from "react";
+import React, { useEffect, useState, useMemo } from "react";
+import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import UseGetData from "../../hooks/UseGet";
 import * as ENV from "../files/ENV.json";
+import axios from "axios";
+
+const UNREAD = "UNREAD";
+const DESCENDING = "desc";
+const ASCENDING = "asc";
+const DATE = "Date";
+const SUBJECT = "Subject";
+const FROM = "From";
+const SNIPPET = "snippet";
+const STRING = "string";
 
 export default function EmailPage() {
+  const history = useHistory();
   const user = useSelector((state) => state.login);
-  const mailsData = UseGetData(
-    ENV.mailsWithOptions + "?q=in:inbox",
+  const [messageDetails, setMessageDetails] = useState([]);
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: null });
+
+  //TODO: remove the unread parameter
+  const [isLoading, mailsData] = UseGetData(
+    ENV.mailsWithOptions + "?q=in:inbox+is:unread",
     user.sanctum_token
-  )[1];
+  );
+
+  useEffect(() => {
+    if (!isLoading && messageDetails.length < mailsData.messages.length) {
+      mailsData.messages.forEach((mail) => {
+        const options = {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json, text/plain, */*",
+            Authorization: "Bearer " + user.sanctum_token,
+          },
+          url: ENV.mails + `/${mail.id}`,
+        };
+
+        const getData = async () => {
+          const response = await axios(options);
+          setMessageDetails((old) => [...old, response.data]);
+        };
+        getData().catch((error) => {
+          if (error.response.status === 422) {
+            return history.push("/login");
+          }
+        });
+      });
+    }
+  }, [isLoading]);
+
+  const sortByField = (key) => {
+    let direction =
+      sortConfig.key === key && sortConfig.direction === DESCENDING
+        ? ASCENDING
+        : DESCENDING;
+    setSortConfig({ key: key, direction: direction });
+  };
+
+  const sortedMessages = useMemo(() => {
+    let sortedMessageDetails = [...messageDetails];
+    if (sortConfig.key !== null) {
+      sortedMessageDetails.sort(
+        orderDetailsByKey(sortConfig.key, sortConfig.direction)
+      );
+    }
+    return sortedMessageDetails;
+  }, [messageDetails, sortConfig]);
 
   return (
     <div className="email_container">
-      {mailsData.messages
-        ? mailsData.messages.map((mail, index) => <p key={index}>{mail.id}</p>)
-        : "Loading"}
+      <table>
+        <thead>
+          <tr>
+            <th onClick={() => sortByField(SUBJECT)}>{SUBJECT}</th>
+            <th onClick={() => sortByField(SNIPPET)}>Message</th>
+            <th onClick={() => sortByField(FROM)}>Author</th>
+            <th onClick={() => sortByField(DATE)}>Created at</th>
+            <th>Delete</th>
+            <th>Mark as unread</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedMessages.map((message) => (
+            <tr
+              key={message.id}
+              style={{
+                fontWeight: message.labelIds.includes(UNREAD) ? "bold" : "",
+              }}
+            >
+              <td>{getDataFromMessage(message, SUBJECT)}</td>
+              <td>{message[SNIPPET]}...</td>
+              <td>{getDataFromMessage(message, FROM)}</td>
+              <td>{getDataFromMessage(message, DATE)}</td>
+              <td>Delete icon</td>
+              <td>Unread icon</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
+
+  function getDataFromMessage(message, dataName) {
+    return message.payload.headers.filter(function (subject) {
+      return subject.name === dataName;
+    })[0].value;
+  }
+
+  function orderDetailsByKey(key, order = DESCENDING) {
+    return function sorter(a, b) {
+      if (
+        key !== SNIPPET &&
+        (a.payload.headers.filter((item) => item.name === key).length < 1 ||
+          b.payload.headers.filter((item) => item.name === key).length < 1)
+      ) {
+        return 0;
+      }
+
+      const valueA =
+        key === DATE
+          ? new Date(
+              a.payload.headers.filter((item) => item.name === key)[0].value
+            )
+          : key === SNIPPET
+          ? a[SNIPPET]
+          : typeof a.payload.headers.filter((item) => item.name === key)[0]
+              .value === STRING
+          ? a.payload.headers
+              .filter((item) => item.name === key)[0]
+              .value.toLowerCase()
+          : a.payload.headers.filter((item) => item.name === key)[0].value;
+      const valueB =
+        key === DATE
+          ? new Date(
+              b.payload.headers.filter((item) => item.name === key)[0].value
+            )
+          : key === SNIPPET
+          ? b[SNIPPET]
+          : typeof b.payload.headers.filter((item) => item.name === key)[0]
+              .value === STRING
+          ? b.payload.headers
+              .filter((item) => item.name === key)[0]
+              .value.toLowerCase()
+          : b.payload.headers.filter((item) => item.name === key)[0].value;
+
+      let compare = 0;
+      if (valueA > valueB) {
+        compare = 1;
+      } else if (valueA < valueB) {
+        compare = -1;
+      }
+      return order === DESCENDING ? compare * -1 : compare;
+    };
+  }
 }
