07a07737382360a6646da312301831e4c7d4cafb
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-03-25 22:25:42 +0100

    Add sorting hook

---
+++ src/hooks/UseSortedData.js
@@ -0,0 +1,32 @@
+import { useMemo, useState } from "react";
+
+const DESCENDING = "desc";
+const ASCENDING = "asc";
+
+const useSortedData = (
+  data,
+  sortingFunction,
+  config = { key: null, direction: null }
+) => {
+  const [sortConfig, setSortConfig] = useState(config);
+
+  const sortedData = useMemo(() => {
+    let sortedData = [...data];
+    if (sortConfig.key !== null) {
+      sortedData.sort(sortingFunction(sortConfig.key, sortConfig.direction));
+    }
+    return sortedData;
+  }, [data, sortConfig]);
+
+  const sortByField = (key) => {
+    let direction =
+      sortConfig.key === key && sortConfig.direction === DESCENDING
+        ? ASCENDING
+        : DESCENDING;
+    setSortConfig({ key: key, direction: direction });
+  };
+
+  return [sortedData, sortByField, sortConfig];
+};
+
+export default useSortedData;
