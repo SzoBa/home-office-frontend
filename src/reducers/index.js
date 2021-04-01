4958ee318da8c85ecea25685059d1aa629c7dd4c
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-04-01 16:54:47 +0200

    Add urlOption reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import actualWeatherReducer from "./actualWeather";
 import backgroundReducer from "./background";
 import locationReducer from "./location";
 import loginReducer from "./login";
+import urlOptionReducer from "./urlOption";
 
 const allReducers = combineReducers({
   login: loginReducer,
   location: locationReducer,
   background: backgroundReducer,
   actualWeather: actualWeatherReducer,
+  urlOption: urlOptionReducer,
 });
 
 export default allReducers;
