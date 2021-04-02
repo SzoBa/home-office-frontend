86a1f1f17f6363022c36930d006b82219681ea54
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-04-02 23:17:31 +0200

    Refactor consts to separate file

---
+++ src/containers/ConstContainer.js
@@ -0,0 +1,8 @@
+export const UNREAD = "UNREAD";
+export const DESCENDING = "desc";
+export const ASCENDING = "asc";
+export const DATE = "Date";
+export const SUBJECT = "Subject";
+export const FROM = "From";
+export const SNIPPET = "snippet";
+export const STRING = "string";
