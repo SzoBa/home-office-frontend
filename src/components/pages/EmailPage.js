4a6005e9e71e34804b05f96e8b80cae70e7d743a
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-04-03 00:03:32 +0200

    Refactor Email page

--- src/components/pages/EmailPage.js
+++ src/components/pages/EmailPage.js
@@ -1,19 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
+import { useSelector } from "react-redux";
 import EmailTable from "../layout/EmailTable";
+import EmailWriteForm from "../layout/EmailWriteForm";
 
 export default function EmailPage() {
-  const [writeEmail, setWriteEmail] = useState(false);
+  const writeReadEmail = useSelector((state) => state.writeReadEmail);
 
   return (
     <div className="email_container">
-      <button
-        onClick={() => {
-          setWriteEmail(!writeEmail);
-        }}
-      >
-        Write
-      </button>
-      {writeEmail ? <div>Csoki</div> : <EmailTable />}
+      {writeReadEmail ? <EmailWriteForm /> : <EmailTable />}
     </div>
   );
 }
