3c75493d949b5bc6ec508142a3e9b58fde7172af
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-04-06 18:06:00 +0200

    Add Footer links

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Footer = (props) => {
   return (
-    <div id="footerContainer">
-      <p>This is the footer</p>
+    <div
+      id="footerContainer"
+      className={
+        useLocation().pathname === "/email" ? "footerEmail" : "footerFull"
+      }
+    >
+      <Link to="/about/impressum">Impressum</Link>
+      <Link to="/about/policy">Privacy Policy</Link>
+      <Link to="/about/about">About</Link>
+      <Link to="/about/contact">Contact</Link>
     </div>
   );
 };
