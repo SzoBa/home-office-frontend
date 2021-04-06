59e47ba573f9968c3f890f6d69899fb70e1e0f11
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-04-06 14:31:52 +0200

    Add basic sending functionalities

--- src/components/layout/EmailWriteForm.js
+++ src/components/layout/EmailWriteForm.js
@@ -1,6 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import UsePostData from "../../hooks/UsePostData";
+import * as ENV from "../files/ENV.json";
 
 const EmailWriteForm = (props) => {
+  const user = useSelector((state) => state.login);
+
+  function sendHandler(event) {
+    event.preventDefault();
+    const emailObject = createEmailObject(event, "emailData");
+    UsePostData(ENV.mails, user.sanctum_token, emailObject, (response) => {
+      // setErrorMessage([]);
+      if (response.status === 201) {
+        console.log(response.data);
+      }
+      // handleErrorMessage(response, setErrorMessage);
+    });
+  }
+
+  function saveHandler(event) {
+    event.preventDefault();
+    console.log("save to drafts, or smthg?");
+  }
+
   return (
     <div className="email_content_container">
       <div>Compose mail</div>
@@ -8,6 +30,14 @@ const EmailWriteForm = (props) => {
         <div>
           <label>Send to user:</label>
           <input type="email" id="address" name="address" />
+        </div>
+        <div>
+          <label>Carbon copy to user:</label>
+          <input type="email" id="carbon_copy" name="carbon_copy" />
+        </div>
+        <div>
+          <label>Blind carbon copy to user:</label>
+          <input type="email" id="blind_carbon" name="blind_carbon" />
         </div>
         <div>
           <label>Subject:</label>
@@ -19,16 +49,24 @@ const EmailWriteForm = (props) => {
           {/* <MDEditor.Markdown source={value} /> */}
         </div>
         <div>
-          <button type="submit" name="save" id="save">
+          <button type="button" onClick={saveHandler} name="save" id="save">
             Save email
           </button>
-          <button type="submit" name="send" id="send">
+          <button type="button" onClick={sendHandler} name="send" id="send">
             Send mail
           </button>
         </div>
       </div>
     </div>
   );
+
+  function createEmailObject(event) {
+    return {
+      name: "mailDataState.address",
+      subject: "mailDataState.subject",
+      message: "mailDataState.message",
+    };
+  }
 };
 
 export default EmailWriteForm;
