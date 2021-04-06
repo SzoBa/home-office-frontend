b4948bd1c500bcfff085c22a3eb56f3973277286
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-04-06 23:39:47 +0200

    Add About page routing, contents

--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -1,52 +1,76 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 
 export default function AboutContainer() {
   return (
     <div className="full_width_container">
-      <AboutContent />
-      <Switch>
-        <Route path="/about/impressum" component={Impressum} />
-        <Route path="/about/policy" component={Policy} />
-        <Route path="/about/about" component={About} />
-        <Route path="/about/contact" component={Contact} />
-      </Switch>
+      <div id="content_div_about">
+        <AboutContent />
+        <Switch>
+          <Route path="/about/impressum" component={Impressum} />
+          <Route path="/about/policy" component={Policy} />
+          <Route path="/about/about" component={About} />
+          <Route path="/about/contact" component={Contact} />
+        </Switch>
+      </div>
     </div>
   );
 }
 
 const Impressum = () => {
   return (
-    <div>
-      <span>Impressum Here</span>
+    <div className="about_inner_content">
+      <div>
+        <div>Impressum</div>
+        <div>Company: Balázs Szolcsánszki for fun :D</div>
+        <div>Creator: Balázs Szolcsánszki</div>
+        <div>
+          API: <Link to="#">http://homeoffice.com</Link>
+        </div>
+        <div>
+          Web: <Link to="#">localhost:3000</Link> :D
+        </div>
+      </div>
     </div>
   );
 };
 
 const Policy = () => {
   return (
-    <div>
-      <span>Policy Here</span>
+    <div className="about_inner_content">
+      <div>Policy</div>
+      <div>Under development...</div>
     </div>
   );
 };
 
 const About = () => {
   return (
-    <div>
-      <span>About Here</span>
+    <div className="about_inner_content">
+      <div>
+        <div>About</div>
+        <div>Design: Balázs Szolcsánszki</div>
+        <div>Front-end programmer: Balázs Szolcsánszki</div>
+        <div>Back-end programmer: Balázs Szolcsánszki</div>
+      </div>
     </div>
   );
 };
 
 const Contact = () => {
   return (
-    <div>
-      <span>Contact Here</span>
+    <div className="about_inner_content">
+      <div>
+        <div>Contact</div>
+        <div>Name: Balázs Szolcsánszki</div>
+        <div>E-mail: bszolcsan@gmail.com</div>
+        <div>Tel.: +3670/287-53-38</div>
+        <div>LinkedIn: </div>
+      </div>
     </div>
   );
 };
 
 const AboutContent = () => {
-  return <div>This is the about page main content</div>;
+  return <h2>INFORMATION</h2>;
 };
