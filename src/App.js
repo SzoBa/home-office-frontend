a062a0f2af149ba695690b1cc801d694e741c281
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-04-07 22:13:17 +0200

    Refactor weather load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import MainContainer from "./containers/MainContainer";
+import WeatherSetter from "./hooks/SetWeather";
 import "./style/app.css";
 
 function App() {
   return (
     <Router>
+      <WeatherSetter />
       <MainContainer />
     </Router>
   );
