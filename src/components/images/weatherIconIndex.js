70310b491865e5061bb6bfb337b0b6e881010b52
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-04-08 19:30:37 +0200

    Add weather icons index file

---
+++ src/components/images/weatherIconIndex.js
@@ -0,0 +1,39 @@
+import wI01d from "./weatherIcons/01d.png";
+import wI01n from "./weatherIcons/01n.png";
+import wI02d from "./weatherIcons/02d.png";
+import wI02n from "./weatherIcons/02n.png";
+import wI03d from "./weatherIcons/03d.png";
+import wI03n from "./weatherIcons/03n.png";
+import wI04d from "./weatherIcons/04d.png";
+import wI04n from "./weatherIcons/04n.png";
+import wI09d from "./weatherIcons/09d.png";
+import wI09n from "./weatherIcons/09n.png";
+import wI10d from "./weatherIcons/10d.png";
+import wI10n from "./weatherIcons/10n.png";
+import wI11d from "./weatherIcons/11d.png";
+import wI11n from "./weatherIcons/11n.png";
+import wI13d from "./weatherIcons/13d.png";
+import wI13n from "./weatherIcons/13n.png";
+import wI50d from "./weatherIcons/50d.png";
+import wI50n from "./weatherIcons/50n.png";
+
+export {
+  wI01d,
+  wI01n,
+  wI02d,
+  wI02n,
+  wI03d,
+  wI03n,
+  wI04d,
+  wI04n,
+  wI09d,
+  wI09n,
+  wI10d,
+  wI10n,
+  wI11d,
+  wI11n,
+  wI13d,
+  wI13n,
+  wI50d,
+  wI50n,
+};
