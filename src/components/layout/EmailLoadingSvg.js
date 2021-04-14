c56a3998dc6e6b2f605d141a281f2286568d613a
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-04-14 18:09:44 +0200

    Add Email Loading svg component

---
+++ src/components/layout/EmailLoadingSvg.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+function EmailLoadingSvgComponent(props) {
+  return (
+    <svg viewBox="0 0 90 120" fill="currentColor" {...props}>
+      <path d="M90 0v120H11c-6.075 0-11-4.925-11-11V11C0 4.925 4.925 0 11 0h79zM71.5 81h-53a2.5 2.5 0 00-.164 4.995L18.5 86h53a2.5 2.5 0 00.164-4.995L71.5 81zm0-24h-53a2.5 2.5 0 00-.164 4.995L18.5 62h53a2.5 2.5 0 100-5zm0-24h-53a2.5 2.5 0 00-.164 4.995L18.5 38h53a2.5 2.5 0 100-5z" />
+    </svg>
+  );
+}
+
+export default EmailLoadingSvgComponent;
