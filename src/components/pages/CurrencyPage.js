e2840da393633089756b9d6c5b28e8744548901e
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-04-28 18:02:00 +0200

    Add currency page

---
+++ src/components/pages/CurrencyPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CurrencyTable from "../layout/CurrencyTable";
+
+export default function CurrencyPage() {
+  return (
+    <div className="full_width_container" style={{ padding: "0" }}>
+      <CurrencyTable />
+    </div>
+  );
+}
